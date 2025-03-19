7019db3d83a839b9941cba2d8e9d1307fc8bd222
kakaz-one <bousomusume2410836@gmail.com>
2025-03-19 17:45:02 +0900

    app/api/employee/[id]のAPIを少し修正

--- src/app/api/employee/[id]/route.ts
+++ src/app/api/employee/[id]/route.ts
@@ -3,9 +3,18 @@ import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function main() {
   try {
     await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗しました");
+  }
+}
+
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  try {
+    await main();
     const employee = await prisma.employeeInfo.findUnique({
       where: {
         id: parseInt(params.id),
